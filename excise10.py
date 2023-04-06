bca2a4870579704378a8c9e8ed56de40b071b642
Kang Soo Hyeon <kangsh5721@naver.com>
2023-04-06 23:52:10 +0900

    eight commit

---
+++ excise10.py
@@ -0,0 +1,13 @@
+a = ["Backjun", "Python", "Coding", "join"]
+
+result1 = "-".join(a)
+print(result1)
+
+result2 = ".".join(a)
+print(result2)
+
+result2 = "*".join(a)
+print(result2)
+
+result2 = "+".join(a)
+print(result2)
